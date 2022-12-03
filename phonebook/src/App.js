dc69423d81710588f12339f8b951d735526651df
João Victor Fanchini de Azevedo <joaofanchini@gmail.com>
2022-12-03 20:02:08 -0300

    Mudança relacionada ao phonebook

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -2,6 +2,14 @@ import {useEffect, useState} from "react";
 import Person from "./components/person/Person";
 import PersonService from "./services/person/person";
 
+const addPhone = (event) => {
+    console.log('Evento add phone',event);
+}
+
+const showPhone = (event) => {
+    console.log('Evento show phone',event);
+}
+
 const App = () => {
     const [persons, setPersons] = useState([]);
 
@@ -12,10 +20,26 @@ const App = () => {
     useEffect(effect, []);
 
     return <>
-        <h1>Persons</h1>
+        <h1>Phonebook</h1>
         {persons.map(person => {
             return <Person person={person} key={person.id}></Person>
         })}
+        <h1>Filter</h1>
+        <div>
+            <form onSubmit={showPhone}>
+                <label for="number">Filter show with </label>
+                <input name="number" type="number"></input>
+                <button type="submit">show</button>
+            </form>
+        </div>
+        <h1>Add new</h1>
+        <form onSubmit={addPhone}>
+            <label for="name">Nome:</label>
+            <input name="name" type="text"></input>
+            <label for="number">Number:</label>
+            <input name="number" type="text"></input>
+            <button type="submit">Add</button>
+        </form>
     </>;
 }
 
